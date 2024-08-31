4943a8f718d67f04f64e95d79a6bea1cb8d9dce9
ashraf56 <ashrafulfahim07@gmail.com>
2024-08-31 16:47:26 +0600

    code modified

--- src/components/HomepageCompo/Hero.tsx
+++ src/components/HomepageCompo/Hero.tsx
@@ -8,7 +8,7 @@ const Hero = () => {
                 <p className=' pb-3 text-lg font-light font-CustomFont uppercase tracking-[0.1m]'> Experience the Road Like Never Before </p>
                 <h1 className='font-bold font-CustomFont uppercase  text-3xl md:text-3xl pb-4 tracking-[0.1m]'>Your Ultimate Destination for Quality Vehicle</h1>
 
-                <div className='flex w-full bg-white h-14 justify-between px-2 gap-2   items-center'>
+                <div className='flex w-full max-w-3xl mx-auto bg-white h-14 justify-between px-2 gap-2   items-center'>
                     <input type="text" placeholder="Search now" className='bg-white h-12 w-full outline-none text-black  px-2' />
                     <Button size={'default'} className=' bg-[#BC274B] h-12' >Book now</Button>
                 </div>
