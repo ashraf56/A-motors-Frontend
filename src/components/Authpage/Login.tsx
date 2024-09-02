5b7d7f97de123436a6f2eb61f9f222fc28da642b
ashraf56 <ashrafulfahim07@gmail.com>
2024-09-02 13:08:58 +0600

    code formated

--- src/components/Authpage/Login.tsx
+++ src/components/Authpage/Login.tsx
@@ -20,50 +20,51 @@ const Login = () => {
     const onsubmit: SubmitHandler<FieldValues> = (data) => console.log(data)
     return (
         <div className="flex flex-col justify-center items-center h-[80vh] my-10 md:my-0">
-        <Card className="w-full max-w-lg">
-            <CardHeader>
-                <CardTitle>Log in now</CardTitle>
-            </CardHeader>
-            <CardContent>
-                <form onSubmit={handleSubmit(onsubmit)}>
-                    <div className="grid w-full items-center gap-4">
-                     
-                        <div className="flex flex-col space-y-1.5 w-full max-w-full">
-                            <Label htmlFor="name">Email</Label>
-                            <Input id="email" type="email" placeholder="your email" {...register('email', {
-                                required: true })} />
-                            {errors.email && <span className="text-sm text-red-700">
-                                {errors.email?.message as string} </span>}
+            <Card className="w-full max-w-lg">
+                <CardHeader>
+                    <CardTitle>Log in now</CardTitle>
+                </CardHeader>
+                <CardContent>
+                    <form onSubmit={handleSubmit(onsubmit)}>
+                        <div className="grid w-full items-center gap-4">
+
+                            <div className="flex flex-col space-y-1.5 w-full max-w-full">
+                                <Label htmlFor="name">Email</Label>
+                                <Input id="email" type="email" placeholder="your email" {...register('email', {
+                                    required: true
+                                })} />
+                                {errors.email && <span className="text-sm text-red-700">
+                                    {errors.email?.message as string} </span>}
+                            </div>
+
+
+                            <div className="flex flex-col space-y-1.5 mb-2 w-full max-w-full">
+                                <Label htmlFor="name">Password</Label>
+                                <Input id="password" type="password" placeholder="enter your password" {...register('password', {
+                                    required: {
+                                        value: true,
+                                        message: 'password is required'
+                                    },
+                                    maxLength: {
+                                        value: 6,
+                                        message: 'Password must be 6 characters'
+                                    },
+                                })} />
+                                {errors.password && <span className="text-sm text-red-700"> {errors.password?.message as string} </span>}
+                            </div>
+
+
                         </div>
-                    
-                        
-                        <div className="flex flex-col space-y-1.5 mb-2 w-full max-w-full">
-                            <Label htmlFor="name">Password</Label>
-                            <Input id="password"  type="password" placeholder="enter your password" {...register('password', {
-                                required: {
-                                    value: true,
-                                    message: 'password is required'
-                                },
-                                maxLength: {
-                                    value: 6,
-                                    message: 'Password must be 6 characters'
-                                },
-                            })} />
-                            {errors.password && <span className="text-sm text-red-700"> {errors.password?.message as string} </span>}
-                        </div>
-                       
-                        
-                    </div>
 
-                    <CardFooter className="flex justify-center mt-10">
-                        <Button type="submit" size={'default'} className="w-full">Log in </Button>
-                    </CardFooter>
+                        <CardFooter className="flex justify-center mt-10">
+                            <Button type="submit" size={'default'} className="w-full">Log in </Button>
+                        </CardFooter>
 
-                </form>
-            </CardContent>
+                    </form>
+                </CardContent>
 
-        </Card>
-    </div>
+            </Card>
+        </div>
     );
 };
 
