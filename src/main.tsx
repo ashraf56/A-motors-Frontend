4945d295b0f1af26cb844042ae5dc9a1a6fa97c9
ashraf56 <ashrafulfahim07@gmail.com>
2024-09-02 16:11:59 +0600

    redux implemented

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,15 @@ import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routers/router.tsx'
 import { Toaster } from './components/ui/sonner.tsx'
+import { Provider } from 'react-redux'
+import { store } from './redux/store.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-
-        <RouterProvider router={router} />
-        <Toaster />
-      
+    <Provider store={store}>
+      <RouterProvider router={router} />
+      <Toaster />
+    </Provider>
 
   </React.StrictMode>,
 )
