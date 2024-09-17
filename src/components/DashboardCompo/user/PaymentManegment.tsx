f90818ca5f2831b46e674a7550f7cbabeca719e3
ashraf56 <ashrafulfahim07@gmail.com>
2024-09-17 17:59:36 +0600

    payment page code updated

--- src/components/DashboardCompo/user/PaymentManegment.tsx
+++ src/components/DashboardCompo/user/PaymentManegment.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import {  useGetmyAllBookingQuery } from "@/redux/feature/bookings/bookingApi";
+import { useGetmyAllBookingQuery } from "@/redux/feature/bookings/bookingApi";
 import {
   Table,
   TableBody,
@@ -22,55 +22,57 @@ const PaymentManegment = () => {
   return (
 
     <div className='mx-auto flex flex-col justify-center items-center container my-6  font-CustomFont'>
-  { data?.data.length === 0 ?<ItemnotFond message={'No payment Available'}/> : <>
-  <div >
-        <h1 className='text-3xl font-bold'>Manage your Payment </h1>
-      </div>
-      <div className='flex flex-col my-5 w-full container gap-4'>
-        <div className=' flex justify-end  w-full'>
+      {data?.data.length === 0 ? <ItemnotFond message={'No payment Available'} /> : <>
+        <div >
+          <h1 className='text-3xl font-bold'>Manage your Payment </h1>
+        </div>
+        <div className='flex flex-col my-5 w-full container gap-4'>
+          <div className=' flex justify-end  w-full'>
+
+          </div>
+
 
         </div>
-
-
-      </div>
-      <div className='w-full   mx-auto justify-center items-center h-96 overflow-y-scroll overflow-x-scroll'>
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead className=''>Date</TableHead>
-              <TableHead className="">Car name</TableHead>
-              <TableHead className="">User name</TableHead>
-              <TableHead className=' text-center '>StartTime</TableHead>
-              <TableHead className='text-center '>End time</TableHead>
-              <TableHead className='text-center '>Booking status</TableHead>
-              <TableHead className='  text-center'>Total cost</TableHead>
-              <TableHead className='  text-center'>Action</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {data?.data?.filter((pm:any) => pm.bookingStatus =='completed').map((p: any) => (
-              <TableRow key={p._id}>
-
-                <TableCell className="font-medium">{p.date}</TableCell>
-                <TableCell className="font-medium">{p.car?.name}</TableCell>
-                <TableCell className="font-medium ">{p.user?.name}</TableCell>
-                <TableCell className="font-medium text-center ">{p.startTime}</TableCell>
-                <TableCell className="font-medium text-center">{p.endTime || 'N/A'}</TableCell>
-                <TableCell className="font-medium text-center ">{p.bookingStatus}</TableCell>
-                <TableCell className="font-medium text-center ">{p.totalCost}</TableCell>
-
-                <TableCell className="font-medium flex gap-3 justify-center   items-center">
-
-                <Button size={'sm'} variant={'outline'}  className="text-red-500"> Complete Payment </Button>        
-                  
-
-                </TableCell>
+        <div className='w-full   mx-auto justify-center items-center h-96 overflow-y-scroll overflow-x-scroll'>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead className=''>Date</TableHead>
+                <TableHead className="">Car name</TableHead>
+                <TableHead className="">User name</TableHead>
+                <TableHead className=' text-center '>StartTime</TableHead>
+                <TableHead className='text-center '>End time</TableHead>
+                <TableHead className='text-center '>Booking status</TableHead>
+                <TableHead className='  text-center'>Total cost</TableHead>
+                <TableHead className='  text-center'>Action</TableHead>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </div>
-   </>}
+            </TableHeader>
+            <TableBody>
+              {data?.data?.map((p: any) => (
+                <TableRow key={p._id}>
+
+                  <TableCell className="font-medium">{p.date}</TableCell>
+                  <TableCell className="font-medium">{p.car?.name}</TableCell>
+                  <TableCell className="font-medium ">{p.user?.name}</TableCell>
+                  <TableCell className="font-medium text-center ">{p.startTime}</TableCell>
+                  <TableCell className="font-medium text-center">{p.endTime || 'N/A'}</TableCell>
+                  <TableCell className="font-medium text-center ">{p.bookingStatus}</TableCell>
+                  <TableCell className="font-medium text-center ">{p.totalCost}</TableCell>
+
+                  <TableCell className="font-medium flex gap-3 justify-center   items-center">
+
+                    {p.bookingStatus === 'completed' ? <Button size={'sm'} variant={'outline'} className="text-red-500"> Complete Payment </Button> :
+                      <Button size={'sm'} variant={'outline'} className="text-red-500" disabled> Complete Payment </Button>
+                    }
+
+
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </div>
+      </>}
     </div>
 
   );
